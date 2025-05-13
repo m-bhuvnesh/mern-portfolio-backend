563bf967beb738783c658c74f299d9af2f427b51
Bhuvnesh Mishra <70465498+m-bhuvnesh@users.noreply.github.com>
2025-05-13 22:32:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // ✅ CORS setup
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"], // Replace with your Vercel frontend domain
+    origin: ["http://localhost:5173", "https://bhuvneshdev.netlify.app/"], // Replace with your Vercel frontend domain
     methods: ["GET", "POST"],
     credentials: true,
   })
